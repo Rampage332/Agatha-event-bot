ed8f8bdf36e4eb5a6a2cf5cdd7ee079515d699d0
Rampage332 <69309750+Rampage332@users.noreply.github.com>
2022-02-11 20:27:03 +0300

    Update init.rb

--- init.rb
+++ init.rb
@@ -4,10 +4,6 @@ require_relative 'partner_list'
 require_relative 'honor_list'
 require 'dotenv/load'
 require 'pry'
-require 'google/apis/sheets_v4'
-require 'googleauth'
-require 'googleauth/stores/file_token_store'
-require 'fileutils'
 
 @agatha_bot = Discordrb::Commands::CommandBot.new(
   token: ENV["TOKEN"],

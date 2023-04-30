07cf73ebcac83982fc628c2f08daeb0696233b5e
Rampage332 <69309750+Rampage332@users.noreply.github.com>
2023-05-01 02:01:26 +0300

    Update init.rb

--- init.rb
+++ init.rb
@@ -72,7 +72,6 @@ def chitchat(user_message)
     # Send to Wit.ai for intent
        #  response = "The message being passed to Wit.ai is: " + user_message
        # response = WIT_CLIENT.message(user_message)
-       response = "testing"
        puts WIT_CLIENT.message(user_message)
     
     if response.nil?

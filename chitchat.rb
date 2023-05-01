5c4331fdb15870c9318cc8753daf05f69cfd988d
Rampage332 <69309750+Rampage332@users.noreply.github.com>
2023-05-01 20:57:14 +0300

    Update chitchat.rb

--- chitchat.rb
+++ chitchat.rb
@@ -8,35 +8,50 @@ WIT_CLIENT = Wit.new(access_token: ENV['WIT_TOKEN'])
 
 # CSV Based Dialog
 
-def chitchat(user_message)
-  # Remove commas, periods, apostrophes, and question marks from the message
-  user_message_stripped = user_message.gsub(/[,.?']/, '').downcase.strip
-  arraycheck = 0
+class ChitChat
   
-  # Search for a matching message in the CSV
-  dialogs = CSV.table("dialogs.csv", converters: :all)
-    dialogs_array = dialogs.find  do |row|
-    row.field(:message) == user_message_stripped
-    end
-  
-  if dialogs_array.nil?
-    # Send to Wit.ai for intent
-       #  response = "The message being passed to Wit.ai is: " + user_message
-       # response = WIT_CLIENT.message(user_message)
-       puts WIT_CLIENT.message(user_message)
-       response = ""
+  def initialize(chat)
     
-    if response.nil?
-      # If no response is found, return a default message
-      return "I'm sorry, I didn't quite understand."
+    @message = chat
+  
+  end
+  
+  def chitchat()
+    # Remove commas, periods, apostrophes, and question marks from the message
+    user_message_stripped = @chat.gsub(/[,.?']/, '').downcase.strip
+    arraycheck = 0
+    sorry = "I'm sorry, I didn't quite understand."
+  
+    # Search for a matching message in the CSV
+    dialogs = CSV.table("dialogs.csv", converters: :all)
+      dialogs_array = dialogs.find  do |row|
+      row.field(:message) == user_message_stripped
+      end
+  
+    if dialogs_array.nil?
+      # Send to Wit.ai for intent
+         #  response = "The message being passed to Wit.ai is: " + user_message
+        # response = WIT_CLIENT.message(user_message)
+        puts WIT_CLIENT.message(@chat)
+        response = ""
+    
+      if response.nil?
+        # If no response is found, return a default message
+        
+        sorry
+        
+      else
+        
+        response
+        
+      end
+    
     else
-      return response
-    end
-    
-  else
-    # If a response is found, return it
-    response = dialogs_array[1]
-    return response
+      # If a response is found, return it
+      response = dialogs_array[1]
+      
+      response
   
+    end
   end
 end

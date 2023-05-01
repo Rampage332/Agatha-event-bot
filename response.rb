88195b4c93ae68a27a9e2e120c278ac8379a51d6
Rampage332 <69309750+Rampage332@users.noreply.github.com>
2023-05-01 23:22:22 +0300

    Update response.rb

--- response.rb
+++ response.rb
@@ -4,10 +4,40 @@ require 'httparty'
 
 # Define Random Fact function ===================================================
 
+def get_chuck_norris_fact
+  url = 'https://api.chucknorris.io/jokes/random'
+  uri = URI(url)
+  response = Net::HTTP.get(uri)
+  json = JSON.parse(response)
+  json['value']
+end
+
+def get_harry_potter_fact
+  url = 'https://hp-api.herokuapp.com/api/characters/random'
+  uri = URI(url)
+  response = Net::HTTP.get(uri)
+  json = JSON.parse(response)
+  json['name']
+end
+
+def get_anime_fact
+  url = 'https://anime-facts-rest-api.herokuapp.com/api/v1/random'
+  uri = URI(url)
+  response = Net::HTTP.get(uri)
+  json = JSON.parse(response)
+  json['data'][0]['fact']
+end
+
 def get_random_fact
-  response = HTTParty.get("https://sciencefacts.io/api/facts?category=all")
-  parsed_response = JSON.parse(response.body)
-  fact = parsed_response["data"].sample["fact"]
+  api_choice = rand(1..3)
+    case api_choice
+    when 1
+     fact = get_chuck_norris_fact
+    when 2
+     fact = get_harry_potter_fact
+   when 3
+     fact = get_anime_fact
+  end
   return fact
 end
 

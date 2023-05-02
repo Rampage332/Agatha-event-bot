fda22f3a2c74fc60d4f801232262f42f8bd90e07
Rampage332 <69309750+Rampage332@users.noreply.github.com>
2023-05-02 22:20:17 +0300

    Update ba_events.rb

--- ba_events.rb
+++ ba_events.rb
@@ -1,35 +1,34 @@
 require 'google/apis/calendar_v3'
 
 class GetEvent
+  
   def initialize(event_name)
-    @event_name = event_name
-    @calendar = Google::Apis::CalendarV3::CalendarService.new
-    @calendar.key = 'AIzaSyDiEL4wGEJJn84x_RxroUj92XtHhbEKTFI'
+    
+    service = Google::Apis::CalendarV3::CalendarService.new
+    service.key = 'AIzaSyDiEL4wGEJJn84x_RxroUj92XtHhbEKTFI'
     @calendar_id = 'rr5boutm86vn02o0dsrkjui4og@group.calendar.google.com'
-    @event = fetch_event
+    @event = service.list_events(@calendar_id, max_results: 100, single_events: true, order_by: 'startTime', time_min: Time.now.iso8601).items.detect { |event| event.summary.downcase.include?(event_name) }
+    @start_time = Time.parse(@event.start.date_time)
+    @end_time = Time.parse(@event.end.date_time)
+
   end
 
+  def title
+    @event.summary
+  end
 
-
-  def fetch_event
-    events = @calendar.list_events(
-        @calendar_id,
-        max_results: 100,
-        single_events: true,
-        order_by: 'startTime',
-        time_min: Time.now.iso8601
-        )
-
-    events.items.each do |event|
-      puts "Summary: #{event.summary}"
-      puts "Description: #{event.description}"
-      puts "Start Time: #{event.start.date_time}"
-      puts "End Time: #{event.end.date_time}"
-      puts "-------------------------"
-    end
-
-    events.items.find { |event| event.summary.downcase.include?(@event_name.downcase) }
+  def starts_on
+    @start_time.strftime("%Y-%m-%d %H:%M:%S")
+  end
   
+  def time_left_tostart
+    seconds = @start_time - Time.now
+    Time.at(seconds).utc.strftime("%H:%M:%S")
+  end
+
+  def time_left_tofinish
+    seconds = @end_time - Time.now
+    Time.at(seconds).utc.strftime("%H:%M:%S")
   end
 
 end

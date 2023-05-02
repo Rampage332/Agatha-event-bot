c9887dfddb1a92e122c3cf589aa55e5cf3a46883
Rampage332 <69309750+Rampage332@users.noreply.github.com>
2023-05-02 20:01:47 +0300

    Update init.rb

--- init.rb
+++ init.rb
@@ -62,9 +62,9 @@ events_list = ['hunting season','adventure path','smilodon attack','strongest ba
     # Send the response back to the user
     if events_list.include? respond
   
-      event_lookup = "brutal age: " + respond
       # Brutal Age events function  ... 
-      ba_event = GetEvent.new(event_lookup.downcase)
+      puts respond
+      ba_event = GetEvent.new(respond.downcase)
       event.channel.send_embed do |embed|
                     embed.colour = 0xFF4000
                     embed.thumbnail = Discordrb::Webhooks::EmbedThumbnail.new(url:ba_event.thumbnail)

3b1db7a2450ae4516195634e35eafff9c508a929
Rampage332 <69309750+Rampage332@users.noreply.github.com>
2023-05-04 13:30:43 +0300

    Update init.rb

--- init.rb
+++ init.rb
@@ -445,7 +445,7 @@ end
         embed.colour = embedcolor
         embed.title = partner.Partner_Title()
         embed.image = Discordrb::Webhooks::EmbedImage.new(url:imageurl)
-        embed.footer = Discordrb::Webhooks::EmbedFooter.new(text: "RAMPAGE #596\n ⚠️ Dislaimer: Guides are a collective effort of the Brutal Age community.", icon_url: "https://i.imgur.com/WQtvk5Z.jpg")
+        embed.footer = Discordrb::Webhooks::EmbedFooter.new(text: "\nRAMPAGE #596\n ⚠️ Disclaimer: Guides are a collective effort of the BA community.", icon_url: "https://i.imgur.com/WQtvk5Z.jpg")
     end
   
 
